b241c003fbb3d5291d7fa2109aae9c5f24d20b02
Shao <shao@abv.bg>
2016-04-14 11:42:27 +0300

    Update JavaFrame1.java

--- src/com/company/JavaFrame1.java
+++ src/com/company/JavaFrame1.java
@@ -1,19 +1,31 @@
 package com.company;
 
-import javafx.scene.control.Cell;
-import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
 
 /**
  * JFrame to hold board.
- * Created by ALEKSANDAR on 4/12/2016.
+ * @author  ALEKSANDAR
  */
-public class JavaFrame1 extends JFrame {
+public class JavaFrame extends JFrame {
+
+    // Indicate whose turn it is
+    private char whoseTurns = 'R';
 
     // Create cell grid
     private final Cell[][] cells = new Cell[4][4];
 
-    public JavaFrame1() {
+    // Create a status label
+    JLabel jlblStatus = new JLabel("'R's turn to play ");
+
+    /**
+     * No - argument Constructor
+     */
+
+    public JavaFrame() {
         // Panel to hold cells
         JPanel panel = new JPanel(new GridLayout(4, 4, 0, 0));
         for (int i = 0; i < 4; i++) {
@@ -21,21 +33,75 @@ public class JavaFrame1 extends JFrame {
                 panel.add(cells[i][j] = new Cell());
             }
         }
+        panel.setBorder(new LineBorder(Color.GREEN, 1));
+        jlblStatus.setBorder(new LineBorder(Color.BLACK, 1));
 
-        /**
-         * Determine if game board is full.
-         *
-         * @return True, if game board is full. Otherwise, false.
-         */
+        add(panel, BorderLayout.CENTER);
+        add(jlblStatus, BorderLayout.SOUTH);
 
+    }
+
+    /**
+     * Determine if game board is full.
+     *
+     * @return True, if game board is full. Otherwise, false.
+     */
     public boolean isFull() {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                if (cells[i][j].getToken() == ' ') {
+                if (cells[i][j].getSymbol() == ' ') {
                     return false;
                 }
             }
         }
         return true;
     }
-}
+
+    /**
+     * Determine if game board is full.
+     *
+     * @param symbol Token to test for winning.
+     * @return True, if the token has won. Otherwise, false.
+     */
+    public boolean isWon(char symbol) {
+        // check rows
+        for (int i = 0; i < 4; i++) {
+            if ((cells[i][0].getSymbol() == symbol)
+                    && (cells[i][1].getSymbol() == symbol)
+                    && (cells[i][2].getSymbol() == symbol)
+                    && (cells[i][3].getSymbol() == symbol)) {
+                return true;
+            }
+        }
+        // check columns
+        for (int j = 0; j < 4; j++) {
+            if ((cells[0][j].getSymbol() == symbol)
+                    && (cells[1][j].getSymbol() == symbol)
+                    && (cells[2][j].getSymbol() == symbol)
+                    && (cells[3][j].getSymbol() == symbol)) {
+                return true;
+            }
+        }
+        // check diagonals
+        if ((cells[0][0].getSymbol() == symbol)
+                && (cells[1][1].getSymbol() == symbol)
+                && (cells[2][2].getSymbol() == symbol)
+                && (cells[3][3].getSymbol() == symbol)) {
+            return true;
+        }
+
+        if ((cells[0][3].getSymbol() == symbol)
+                && (cells[1][1].getSymbol() == symbol)
+                && (cells[2][2].getSymbol() == symbol)
+                && (cells[3][0].getSymbol() == symbol)) {
+            return true;
+
+        }if ((cells[3][3].getSymbol() == symbol)
+                && (cells[2][2].getSymbol() == symbol)
+                && (cells[1][1].getSymbol() == symbol)
+                && (cells[0][0].getSymbol() == symbol)) {
+            return true;
+
+        }
+        return false;
+    }
